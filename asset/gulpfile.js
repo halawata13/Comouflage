e7e07defaa02f262570a5549c80d34d76024d20a
halawata13 <rsuzuki13@gmail.com>
2017-02-15 15:33:03 +0900

    Update gulpfile.

--- asset/gulpfile.js
+++ asset/gulpfile.js
@@ -1,6 +1,11 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var babel = require("gulp-babel");
+var webpack = require('gulp-webpack');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require("browser-sync").create();
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 
 gulp.task('sass', function() {
     gulp.src([
@@ -14,12 +19,47 @@ gulp.task('sass', function() {
             includeContent: false,
             sourceRoot: '../src/css/'
         }))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('watch', ['sass'], function() {
+gulp.task('babel', function() {
+    gulp.src("src/js/*.js")
+        .pipe(webpack({
+            output: {
+                filename: 'main.js',
+            }
+        }))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('compress', function(cb) {
+    pump([
+            gulp.src('dist/js/*.js'),
+            uglify(),
+            gulp.dest('dist/js')
+        ],
+        cb
+    );
+});
+
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('watch', ['sass', 'babel'], function() {
+
+    browserSync.init({
+        proxy: 'localhost:8000'
+    });
 
     gulp.watch("src/css/**/*.scss", ['sass']);
+    gulp.watch("src/js/**/*.js", ['babel']);
 });
 
 gulp.task('default', ['watch']);
+gulp.task('build', ['sass', 'babel', 'compress']);
